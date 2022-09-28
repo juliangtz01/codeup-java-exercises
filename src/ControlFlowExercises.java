734cc3856604025ade3b51bf0a103bd5d218b18e
Julian Gutierrez <julian.milton.gutierrez@gmail.com>
2022-09-28 14:03:25 -0500

    completed the FizzBuzz exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -89,5 +89,80 @@ public class ControlFlowExercises {
             }
             num *= num;
         }
+
+        /**
+         * 2. Fizzbuzz
+         *
+         * One of the most common interview questions for entry-level programmers is the FizzBuzz test. Developed by Imran Ghory, the test is designed to assess basic looping and conditional logic skills.
+         *
+         * *    Write a program that prints the numbers from 1 to 100.
+         * *    For multiples of three: print “Fizz” instead of the          number.
+         * *    For the multiples of five: print “Buzz”.
+         * *    For numbers which are multiples of both three and five:      print “FizzBuzz”.
+         **/
+        System.out.println();
+        for(int x = 1; x <= 100; x++)
+        {
+            if(x % 3 == 0 && x % 5 == 0)
+            {
+                System.out.println("FizzBuzz");
+            }
+            else if(x % 3 == 0)
+            {
+                System.out.println("Fizz");
+            }
+            else if(x % 5 == 0)
+            {
+                System.out.println("Buzz");
+            }
+            else
+            {
+                System.out.println(x);
+            }
+        }
+
+         /**
+          ** 3. Display a table of powers.
+         *
+         * *    Prompt the user to enter an integer.
+         * *    Display a table of squares and cubes from 1 to the           value entered.
+         *      Ask if the user wants to continue.
+         *      Assume that the user will enter valid data.
+         *      Only continue if the user agrees to.
+          *
+         * Example Output
+         *
+         *
+         * What number would you like to go up to? 5
+         *
+         * Here is your table!
+         *
+         * number | squared | cubed
+         * ------ | ------- | -----
+         * 1      | 1       | 1
+         * 2      | 4       | 8
+         * 3      | 9       | 27
+         * 4      | 16      | 64
+         * 5      | 25      | 125
+          **/
+
+         /** Convert given number grades into letter grades.
+         *
+         **  Prompt the user for a numerical grade from 0 to 100.
+         **  Display the corresponding letter grade.
+         **  Prompt the user to continue.
+         **  Assume that the user will enter valid integers for the       grades.
+         **  The application should only continue if the user agrees to.
+         **  Grade Ranges:
+         *
+         * A : 100 - 88
+         * B : 87 - 80
+         * C : 79 - 67
+         * D : 66 - 60
+         * F : 59 - 0
+         * Bonus
+         *
+         * Edit your grade ranges to include pluses and minuses (ex: 99-100 = A+).
+         **/
     }
 }

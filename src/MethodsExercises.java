2166897d011f2874fb5fed6d7059ff9ab78b277d
Julian Gutierrez <julian.milton.gutierrez@gmail.com>
2022-10-03 12:00:40 -0500

    completed problem 2

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class MethodsExercises
 {
     public static void main(String[] args)
@@ -10,6 +12,9 @@ public class MethodsExercises
         System.out.printf("Multiplying %d and %d will result in %d.%n",num1,num2,multiply(num1, num2));
         System.out.printf("Dividing %d and %d will result in %d.%n",num1, num2,divide(num1, num2));
         System.out.printf("When dividing %d and %d, the remainder is %d.%n", num1, num2, remainder(num1, num2));
+        System.out.println();
+        System.out.print("Enter a number between 1 and 10: ");
+        int userInput = getInteger(1, 10);
 
 
     }
@@ -42,6 +47,25 @@ public class MethodsExercises
     public static int remainder(int num1, int num2)
     {
         return num1 % num2;
+    }
+
+    //Create a method that validates that user input is in a certain range and returns that input as an integer if it is within the given (1 - 10) range. If not, prompt the user to input their number again until the input is within range.
+    public static int getInteger(int min, int max)
+    {
+        Scanner scanner = new Scanner(System.in);
+        int result = scanner.nextInt();
+
+        if(result >=1 && result <= 10)
+        {
+            System.out.printf("The number you entered was %d.%n", result);
+            return result;
+        }
+        else
+        {
+            System.out.print("Enter a number between 1 and 10: ");
+            getInteger(1, 10);
+            return 0;
+        }
     }
 
 

97083031e26d7f47d382ca5c8d1f5b4b4f14f0d6
Julian Gutierrez <julian.milton.gutierrez@gmail.com>
2022-10-03 15:50:13 -0500

    completed string basics exerices

---
+++ src/StringExercise.java
@@ -0,0 +1,16 @@
+public class StringExercise
+{
+    public static void main(String[] args)
+    {
+        String value1 = "We don't need no education";
+        System.out.printf("%s%n", value1);
+        System.out.printf("%s%n",value1.replace("education", "thought control"));
+        String value2 = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.printf("%s%n",value2);
+        String value3 = "In windows, the main drive is usually c:\\";
+        System.out.printf("%s%n",value3);
+        String value4 = "I can do backslashes \\, double backslashes \\\\, and the amazing triple backslash \\\\\\";
+        System.out.printf("%s%n",value4);
+
+    }
+}

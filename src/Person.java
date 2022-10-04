f475eda12ff6bb60bed26bd7788307d09d0bb72c
Julian Gutierrez <julian.milton.gutierrez@gmail.com>
2022-10-04 14:54:14 -0500

    completed Object basics problem 1, Object Oriented Programming

---
+++ src/Person.java
@@ -0,0 +1,57 @@
+public class Person
+{
+   private String name;
+
+   /**
+    * The class should have a constructor that accepts a `String` value and sets
+    * the person's name to the passed string.
+    *
+    * Create a `main` method on the class that creates a new `Person` object and
+    * tests the above methods.
+    **/
+
+   public Person(String name)
+   {
+       this.name = name;
+   }
+
+    public String getName()
+    {
+        //TODO: return the person's name
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        //TODO: change the name field to the passed value
+        this.name = name;
+    }
+    public void sayHello()
+    {
+        //TODO: print a message to the console using the person's name
+        System.out.printf("Hi, my name is %s!%n", name);
+    }
+
+    public static void main(String[] args)
+    {
+        Person julian = new Person("Julian");
+        julian.sayHello();
+//
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person1 == person2);
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2);
+
+        Person person1 = new Person("John");
+        Person person2 = person1;
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+        person2.setName("Jane");
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+    }
+}

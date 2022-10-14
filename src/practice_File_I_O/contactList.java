81629a025e4243abaa036f74e49a322b6f0bdb48
Julian Gutierrez <julian.milton.gutierrez@gmail.com>
2022-10-14 00:45:07 -0500

    working on the File I/O

--- src/practice_File_I_O/contactList.java
+++ src/practice_File_I_O/contactList.java
@@ -1,6 +1,10 @@
 package practice_File_I_O;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Scanner;
 
 public class contactList
@@ -10,23 +14,9 @@ public class contactList
         try
         {
             Scanner scanner = new Scanner(System.in);
-            File file = new File("src/practice_File_I_O/contacts.txt");
-            if(file.createNewFile())
-            {
-                System.out.printf("File created: %s",file.getName());
-            }
-            else
-            {
-                System.out.println("File already exists!");
-            }
+//            File file = new File("src/practice_File_I_O/contacts.txt");
 
-            FileWriter myWriter = new FileWriter(file.getName());
-
-            BufferedWriter writer = new BufferedWriter(myWriter);
-
-            FileReader myReader = new FileReader(file.getName());
-
-            BufferedReader br = new BufferedReader(myReader);
+            BufferedReader br = new BufferedReader(new FileReader("src/practice_File_I_O/contacts.txt"));
 
 
             System.out.println("1. View Contacts.\n2. Add a new contact.\n3. Search a contact by name.\n4. Delete an existing contact.\n5. Exit");
@@ -36,10 +26,24 @@ public class contactList
             switch (userInput)
             {
                 case 1:
-                    if(!br.ready())
+                    System.out.println();
+                    BufferedReader fileReader = new BufferedReader(new FileReader("src/practice_File_I_O/contacts.txt"));
+
+                    String lineTxt = fileReader.readLine();
+
+                    if(lineTxt == null)
                     {
-                        System.out.println("Your text file is empty!");
+                        System.out.println("Contact list is empty");
                     }
+                    else
+                    {
+                        while(lineTxt != null)
+                        {
+                            System.out.println(lineTxt);
+                            lineTxt = fileReader.readLine();
+                        }
+                    }
+                    fileReader.close();
                     break;
                 case 2:
                     scanner.nextLine();
@@ -48,26 +52,86 @@ public class contactList
                     String name = scanner.nextLine();
                     System.out.print("\nEnter the new contact's phone number: ");
                     String phoneNumber = scanner.next();
-                    writer.write(name + "  " + phoneNumber);
+                    String contact = "\n" + name + "  " + phoneNumber;
+                    FileWriter fw = new FileWriter("src/practice_File_I_O/contacts.txt", true);
+                    BufferedWriter bw = new BufferedWriter(fw);
+                    bw.write(contact);
+                    bw.close();
+
                     break;
                 case 3:
+                    scanner.nextLine();
+                    String input;
+                    int length = 0;
                     System.out.print("Enter the name of the contact you want to search for: ");
                     String searchName = scanner.nextLine();
-                    while(br.readLine() != null)
+                    BufferedReader nameSearch = new BufferedReader(new FileReader("src/practice_File_I_O/contacts.txt"));
+                    input = nameSearch.readLine();
+                    length = input.length();
+//                    String[][] arr = {input.split("|",1)};
+//                    String[] arr = new String[input.length() - 1];
+                    System.out.println(input.length());
+
+                    int count = 0;
+                    while(input != null)
                     {
-                        System.out.println(br);
+                        input = nameSearch.readLine();
+//                        test[count] = String.valueOf(input.split("|",1));
+                        count++;
                     }
+
+                    String[] test = new String[4];
+
+                    System.out.println(count);
+
+                    int count2 = 0;
+
+                    BufferedReader search = new BufferedReader(new FileReader("src/practice_File_I_O/contacts.txt"));
+                    String input2 = search.readLine();
+
+                    while(input2 != null)
+                    {
+                        test[count2] = Arrays.toString(input2.split("|", 1));
+                        input2 = search.readLine();
+                        count2++;
+                    }
+
+                    count2 = 0;
+
+
+//                    for(int x = 0; x < test.length; x++)
+//                    {
+//                        System.out.println(Arrays.deepToString(test));
+//                    }
+
+                    String n = "";
+
+                    for(String a : test)
+                    {
+                        if(a.contains(searchName))
+                        {
+//                            System.out.println(a);
+                            n = Arrays.toString(a.split("|", 1));
+                            break;
+                        }
+                        else
+                        {
+                            n = "Contact " + searchName + " is not in the contact list!";
+                        }
+                    }
+                    System.out.println(n);
+                    nameSearch.close();
+                    search.close();
                     break;
                 default:
             }
 
-
-
-
-            myReader.close();
-            myWriter.close();
             br.close();
-            writer.close();
+        }
+        catch(FileNotFoundException e)
+        {
+            System.out.println("File does not exits");
+            e.printStackTrace();
         }
         catch(Exception e)
         {
